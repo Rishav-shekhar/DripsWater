ae7b355e728ff8fb55d66ca2d208e5bcdbd889a5
nbtgit2020 <demodemo201888@gmail.com>
2025-03-06 10:40:26 +0530

    first commit

--- UserService/src/main/java/UserService/UserServiceApplication.java
+++ UserService/src/main/java/UserService/UserServiceApplication.java
@@ -8,6 +8,8 @@ public class UserServiceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(UserServiceApplication.class, args);
+
+		System.out.println("run");
 	}
 
 }
